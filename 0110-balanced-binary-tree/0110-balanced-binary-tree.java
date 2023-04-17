2bef64d4e737ec1f5dde1e68c8ea8a8c5cf33361
virat122 <121007372+virat122@users.noreply.github.com>
2023-04-18 02:09:10 +0530

    Time: 1 ms (93.44%), Space: 42.7 MB (25.30%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,26 +14,41 @@
  * }
  */
 class Solution {
-
-    public int hieght(TreeNode root){
-        if(root == null )return 0;
-
-        int l=hieght(root.left);
-        int r=hieght(root.right);
-        return( Math.max(l,r))+1;
-    
-    }
+        
+      
     public boolean isBalanced(TreeNode root) {
-        if(root== null) return true;
-        
-            
-          int left= hieght( root.left);
-          int  right=  hieght( root.right);
-          if(Math.abs(left-right)>=2)return
-          false;
-        
+        if(root==null)
+            return true;
+        if(Math.abs(maxDepth(root.left)-maxDepth(root.right))>1)
+            return false;
         return isBalanced(root.left)&&isBalanced(root.right);
+    }
+    public int maxDepth(TreeNode root){
+        if(root==null)
+            return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+
+
+//     public int hieght(TreeNode root){
+//         if(root == null )return 0;
+
+//         int l=hieght(root.left);
+//         int r=hieght(root.right);
+//         return( Math.max(l,r))+1;
+    
+//     }
+//     public boolean isBalanced(TreeNode root) {
+//         if(root== null) return true;
+        
+            
+//           int left= hieght( root.left);
+//           int  right=  hieght( root.right);
+//           if(Math.abs(left-right)>=2)
+//                   return false;
+         
+//         return isBalanced(root.left)&&isBalanced(root.right);
             
         
-    }
+//     }
 }
