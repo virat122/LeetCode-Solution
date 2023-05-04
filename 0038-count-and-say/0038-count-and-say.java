cd396167e4c56daf19da5af9fadc4a16e79d66e7
virat122 <121007372+virat122@users.noreply.github.com>
2023-05-05 01:04:12 +0530

    Time: 2 ms (99.12%), Space: 40.3 MB (72.84%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -3,6 +3,7 @@ class Solution {
             String s="1";
             for(int i=2;i<=n;i++){
                     s=soln(s);
+                 //   System.out.println(s);
                   
             }
             return s;
@@ -11,22 +12,25 @@ class Solution {
         
     }
         public String soln(String s){
-                String ans="";
+                StringBuilder ans=new StringBuilder("");
                 int i=1;
                 int c=1;
-                while(i<s.length()){
+                int n=s.length();
+              while(i<n){
                         if(s.charAt(i-1)==s.charAt(i))c++;
                         else{
-                                ans+=c;
-                               ans+=s.charAt(i-1); 
+                                ans.append(c);
+                                ans.append(s.charAt(i-1));
+                              
                                 c=1;
                         }
                         i++;
                       
                 }
-                ans+=c;
-                ans+=s.charAt(s.length()-1);
-                return ans;
+                  ans.append(c);
+                  ans.append(s.charAt(n-1));
+              
+                return ans.toString();
                 
                 
                 
