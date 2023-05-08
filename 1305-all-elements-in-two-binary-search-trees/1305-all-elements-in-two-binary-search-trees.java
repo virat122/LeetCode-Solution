fd21dd264a90e93548bbe5b37c49839fc1b02b39
virat122 <121007372+virat122@users.noreply.github.com>
2023-05-08 16:34:56 +0530

    Time: 18 ms (91.21%), Space: 46 MB (14.98%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -16,11 +16,39 @@
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
             List<Integer> a1=new ArrayList<Integer>();
-           
+            List<Integer> a2=new ArrayList<Integer>();
+            List<Integer> ans=new ArrayList<Integer>();
             pre(root1,a1);
-              pre(root2,a1);
-            Collections.sort(a1);
-            return a1;
+            pre(root2,a2);
+            int j= 0,i=0;
+            int n1=a1.size();
+             int n2=a2.size();
+            
+            
+            while(i<n1 && j<n2){
+                    if(a1.get(i)<=a2.get(j)){
+                            ans.add(a1.get(i));
+                            i++;
+                    }else{
+                           ans.add(a2.get(j)); 
+                            j++;
+                    }
+            }
+             while(i<n1 ){
+                   
+                            ans.add(a1.get(i));
+                            i++;
+                   
+            }
+             while(j<n2 ){
+                   
+                            ans.add(a2.get(j));
+                            j++;
+                   
+            }
+            
+           // Collections.sort(a1);
+            return ans;
             
                     
     }
