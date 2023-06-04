0d9c290fe4c64fbe2176670e46cc1a1849cbff20
virat122 <121007372+virat122@users.noreply.github.com>
2023-06-05 00:10:47 +0530

    Added solution - LeetHub

---
+++ Find the Closest Element in BST - GFG/find-the-closest-element-in-bst.java
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Solution T = new Solution();
+            System.out.println(T.minDiff(root,k));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+*/
+
+
+
+//User function Template for Java
+
+/*
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+*/
+
+class Solution
+{
+    //Function to find the least absolute difference between any node
+    //value of the BST and the given integer.
+    static int minDiff(Node  root, int K) 
+    { 
+        // Write your code here
+        int res = K;
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        while(q.isEmpty() == false){
+            int cnt = q.size();
+            for(int i=0;i<cnt;i++){
+                Node curr = q.poll();
+                
+                res = Math.min(res, Math.abs(curr.data-K));
+                
+                //skipping one part of the tree reducing tc to O(logn) form O(n) 
+                if( ( K < curr.data ) && curr.left!=null)
+                    q.add(curr.left);
+                else if( ( K>curr.data ) && curr.right!=null)
+                    q.add(curr.right);
+            }
+        }
+        
+        return res;
+    } 
+}
+
