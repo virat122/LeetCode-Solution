bf6e4bae708fb2a5087ac573f1bc4f002f85655d
Vikrant Chaudhary <121007372+virat122@users.noreply.github.com>
2023-09-18 00:05:52 +0530

    Time: 2 ms (64.74%), Space: 51.7 MB (83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,5 +1,34 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+          // Approach - 2 (Space saved)
+        // Time - O(n) & Space - O(1)
+        
+        int length = nums.length;
+        
+        int[] answer = new int[length];
+        
+        //  Initially answer array holds product of all elements to the left
+        answer[0] = 1;
+        for(int i = 1; i < length; i++){
+            answer[i] = answer[i - 1] * nums[i - 1]; 
+        }
+     
+        int R = 1;
+        
+        for(int i = length - 1; i >= 0; i--){
+   
+            answer[i] = answer[i] * R;
+      
+            R = R * nums[i];
+        }
+        
+        return answer;
+        
+
+        
+       
+    }
+     public int[] SPACEENEFFECTIVE(int[] nums) {
         int preMul[]=new int [nums.length];
         int sufMul[]=new int [nums.length];
         int ans[]=new int [nums.length];
@@ -27,18 +56,6 @@ class Solution {
             }
         }
 return ans;
-
-       
-
-
-
-        
-
-    
-
-
-
-        
-        
+ 
     }
 }
