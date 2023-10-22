4f07911cfacdf38230023f17c0f14b7e7544ad7d
Vikrant Chaudhary <121007372+virat122@users.noreply.github.com>
2023-10-23 01:44:05 +0530

    Time: 0 ms (100%), Space: 39.5 MB (28.29%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -1,15 +1,22 @@
 class Solution {
     public int numTilings(int n) {
-        long arr[]=new long [n+3];
+        int  arr[]=new int [n+3];
         arr[1]=1;
         arr[2]=2;
         arr[3]=5;
+
+        for(int i=4;i<=n;i++){
+            arr[i]=(2*arr[i-1] %1000000007+arr[i-3] )%1000000007;
+
+        }
+
      
         
-        return (int)nTilings(n,arr);
+        return arr[n];
         
     }
-     public long nTilings(int n,long []arr) {
+
+    public long nTilings(int n,long []arr) {
          if(n<4){
              return arr[n];
          }
@@ -22,4 +29,17 @@ class Solution {
          return arr[n];
         
     }
+    //  public long  memorizationnTilings(int n,long []arr) {
+    //      if(n<4){
+    //          return arr[n];
+    //      }
+    //     if(arr[n]!=0)return arr[n];
+       
+
+
+
+    //      arr[n]=(2*nTilings(n-1,arr)+nTilings(n-3,arr))%1000000007;
+    //      return arr[n];
+        
+    // }
 }
